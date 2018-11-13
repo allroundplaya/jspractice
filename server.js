23855af009aa4609d22858a7ffdc4e2ce4b74d58
Dev_ES <anderson93@naver.com>
2018-11-13 19:55:11 +0900

    using response.write() function

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ var http = require("http"); // using http module
 // 함수 자체가 1급 객체.
 // creating server using anonymous function
 // 200 is "OK"
+
+console.log("Testing Node.js Server...")
+
+
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/html"});
   response.write("<h2>Hello World</h2>");
+  response.write("<h1>this is korea</h1>");
+  
+
   response.end();
 
 }).listen(8888); // it will listen by(?) port 8888
